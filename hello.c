a017de01e638b6ba794ae74af415575a2459d8d1
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2024-12-23 01:17:04 +0100

    Introducción método resta

--- hello.c
+++ hello.c
@@ -4,8 +4,13 @@ int suma(int a, int b) {
     return a + b;
 }
 
+int resta(int a, int b) {
+    return a - b;
+}
+
 int main() {
     printf("Bye, World!\n");
     printf("Suma: %d\n", suma(2, 3));
+    printf("Resta: %d\n", resta(2, 3));
     return 0;
 }
