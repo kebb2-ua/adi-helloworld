4ea2c780f4abbc1ef3f2d430f0583afa58f921ec
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2024-12-23 01:22:06 +0100

    Introducción método multiplicación

--- hello.c
+++ hello.c
@@ -8,9 +8,14 @@ int resta(int a, int b) {
     return a - b;
 }
 
+int multiplicacion(int a, int b) {
+    return a * b;
+}
+
 int main() {
     printf("Bye, World!\n");
     printf("Suma: %d\n", suma(2, 3));
     printf("Resta: %d\n", resta(2, 3));
+    printf("Multiplicacion: %d\n", multiplicacion(2, 3));
     return 0;
 }
