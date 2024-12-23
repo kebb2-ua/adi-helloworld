9d5bfc34258331951b2cf17f6adc36d62e9e9cc3
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2024-12-23 01:23:27 +0100

    Fusión suma/resta

--- hello.c
+++ hello.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int suma(int a, int b) {
-    return a + b;
-}
-
-int resta(int a, int b) {
-    return a - b;
+int sumaOresta(int a, int b, int op) {
+    if (op == 1) {
+        return a + b;
+    } else {
+        return a - b;
+    }
 }
 
 int multiplicacion(int a, int b) {
@@ -18,8 +18,8 @@ int cuadrado(int a) {
 
 int main() {
     printf("Bye, World!\n");
-    printf("Suma: %d\n", suma(2, 3));
-    printf("Resta: %d\n", resta(2, 3));
+    printf("Suma: %d\n", sumaOresta(2, 3, 1));
+    printf("Resta: %d\n", sumaOresta(2, 3, 0));
     printf("Multiplicacion: %d\n", multiplicacion(2, 3));
     printf("Cuadrado: %d\n", cuadrado(2));
     return 0;
