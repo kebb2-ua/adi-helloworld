402c8c75d7353c3c6d934bc842ef77ab7d2b91aa
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2024-12-23 01:22:39 +0100

    Introducción método cuadrado

--- hello.c
+++ hello.c
@@ -12,10 +12,15 @@ int multiplicacion(int a, int b) {
     return a * b;
 }
 
+int cuadrado(int a) {
+    return a * a;
+}
+
 int main() {
     printf("Bye, World!\n");
     printf("Suma: %d\n", suma(2, 3));
     printf("Resta: %d\n", resta(2, 3));
     printf("Multiplicacion: %d\n", multiplicacion(2, 3));
+    printf("Cuadrado: %d\n", cuadrado(2));
     return 0;
 }
